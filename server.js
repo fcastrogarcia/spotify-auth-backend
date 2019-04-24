0cb9ae3841e2819ac4794637867b231d0967d4a6
fcastrogarcia <fede.castrog@gmail.com>
2019-04-23 23:34:17 -0300

    refiling

--- server.js
+++ server.js
@@ -1,14 +1,10 @@
 const dotenv = require('dotenv').config()
 const express = require('express')
 const app = express() 
-const authRouter = require('./auth')
-
-
+const authRouter = require('./auth/auth.routes')
 
 app.use('/', authRouter)
   
-
-
 
 const PORT = process.env.PORT
 app.listen(PORT, console.log(`Server is running on port ${PORT}`))
