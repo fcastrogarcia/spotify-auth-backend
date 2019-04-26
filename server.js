e9de1b9a9d3c2d55bcce501c86ab33af64d2b85a
fcastrogarcia <fede.castrog@gmail.com>
2019-04-26 00:09:10 -0300

    redirect with tokens

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
 const dotenv = require('dotenv').config()
 const express = require('express')
 const app = express() 
-const authRouter = require('./auth/auth.routes')
+const querystring = require('querystring')
+const axios = require('axios')
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
 
-app.use('/', authRouter)
-  
+const authRoutes = require('./auth/authRoutes')
+
+
+app.use(cors())
+   .use(cookieParser());
+
+authRoutes(app)
+
+ 
 
 const PORT = process.env.PORT
 app.listen(PORT, console.log(`Server is running on port ${PORT}`))
